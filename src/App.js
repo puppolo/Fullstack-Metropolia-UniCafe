12605e0a4dc5150a9d1da618b6cd58271718a124
PupPolo <killmasterjoy13@live.com>
2022-03-25 14:04:40 +0200

    exercise 1.11 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Result from './Result'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Statistics = (props) => {
+  if(props.good == 0 && props.neutral == 0 && props.bad == 0) {
+    return (
+      <div>
+      <h1>statistics</h1>
+      <p>No feedback given</p>
+      </div>
+    )
+  }
+  else {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <table>
+          <Result name={'good'} count={props.good} />
+          <Result name={'neutral'} count={props.neutral} />
+          <Result name={'bad'} count={props.bad} />
+          <Result name={'all'} count={props.all} />
+          <Result name={'average'} count={props.average} />
+          <Result name={'positive'} count={props.positive} post={'%'} />
+        </table>
+      </div>  
+    )
+  }
 }
 
-export default App;
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+  const all = good+bad+neutral
+  const average = ((good-bad)/all).toFixed(1)
+  const positive = (good*100/all).toFixed(1)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <div>
+        <button onClick={() => setGood(good + 1)}>good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+        <button onClick={() => setBad(bad + 1)}>bad</button>
+      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive} />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Result.js
@@ -0,0 +1,7 @@
+function Result(props) {
+    return (
+        <tr>{props.name} {props.count} {props.post}</tr>
+    )
+}
+
+export default Result;
